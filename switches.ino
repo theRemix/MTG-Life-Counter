ef74cf13d7896e8cf2871ae1b9f5d33020ac3582
Bjorn Borgonia <bjorn@easci.com>
2018-01-30 16:11:10 -0800

    2 button capability

--- switches.ino
+++ switches.ino
@@ -1,24 +1,38 @@
-const int inPin = 2;
+const int btn1 = 2;
+const int btn2 = 3;
 
-bool pressed = false;
-int reading;
+int btn1down;
+bool btn1pressed = false;
+int btn2down;
+bool btn2pressed = false;
 
 void setup() {
   Serial.begin(9600);
 
   Serial.print("Ready, on 9600");
 
-  pinMode(inPin, INPUT);
+  pinMode(btn1, INPUT);
+  pinMode(btn2, INPUT);
 }
 
 void loop() {
-  reading = digitalRead(inPin);
+  btn1down = digitalRead(btn1);
+  btn2down = digitalRead(btn2);
 
-  if (reading == HIGH && !pressed) {
-    pressed = true;
-    Serial.println("PRESSED 2");
-  } else {
-    pressed = false;
-    delay(50);
+  if (!btn1pressed && btn1down == HIGH) {
+      Serial.println("PRESSED 1");
+      btn1pressed = true;
+  } else if(btn1down == LOW){
+      btn1pressed = false;
+      delay(50);
   }
+
+  if (!btn2pressed && btn2down == HIGH) {
+      Serial.println("PRESSED 2");
+      btn2pressed = true;
+  } else if(btn2down == LOW){
+      btn2pressed = false;
+      delay(50);
+  }
+
 }
